b4064246fee0287580b014a3dff56bb92b514a31
kimkimice <151143655+kimkimice@users.noreply.github.com>
2024-08-06 16:52:14 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import time
 import os
-import sys
 import cv2
 import queue
 import threading
 import numpy as np
 from datetime import datetime
-from skimage.metrics import mean_squared_error as ssim
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter, BooleanOptionalAction
 from sshkeyboard import listen_keyboard, stop_listening
 import mysql.connector
@@ -21,30 +19,10 @@ parser.add_argument("--yolo", type=str,
                     help="Enables YOLO object detection. Enter a comma separated list of objects you'd like the program to record. The list can be found in the coco.names file")
 parser.add_argument("--model", default='yolov8n', type=str,
                     help="Specify which model size you want to run. Default is the nano model.")
-parser.add_argument("--threshold", default=350, type=int, choices=range(1, 10000),
-                    help="Determines the amount of motion required to start recording. Higher values decrease sensitivity to help reduce false positives. Default 350, max 10000.")
-parser.add_argument("--start_frames", default=3, type=int, choices=range(1, 30),
-                    help="Number of consecutive frames with motion required to start recording. Raising this might help if there's too many false positive recordings, especially with a high frame rate stream of 60 FPS. Default 3, max 30.")
-parser.add_argument("--tail_length", default=8, type=int, choices=range(1, 30),
-                    help="Number of seconds without motion required to stop recording. Raise this value if recordings are stopping too early. Default 8, max 30.")
-parser.add_argument('--testing', default=False, action=BooleanOptionalAction,
-                    help="Testing mode disables recordings and prints out the motion value for each frame if greater than threshold. Helps fine tune the threshold value.")
-parser.add_argument('--frame_click', default=False, action=BooleanOptionalAction,
-                    help="Allows user to advance frames one by one by pressing any key. For use with testing mode on video files, not live streams, so set a video file instead of an RTSP address for the --stream argument.")
 args = vars(parser.parse_args())
 
 rtsp_stream = args["stream"]
 monitor = args["monitor"]
-thresh = args["threshold"]
-start_frames = args["start_frames"]
-tail_length = args["tail_length"]
-testing = args["testing"]
-frame_click = args["frame_click"]
-if frame_click:
-    testing = True
-    monitor = True
-    print(
-        "frame_click enabled. Press any key to advance the frame by one, or hold down the key to advance faster. Make sure the video window is selected, not the terminal, when advancing frames.")
 if args["yolo"]:
     yolo_list = [s.strip() for s in args["yolo"].split(",")]
     yolo_on = True
@@ -77,10 +55,7 @@ loop = True
 cap = cv2.VideoCapture(rtsp_stream)
 fps = cap.get(cv2.CAP_PROP_FPS)
 period = 1 / fps
-tail_length = tail_length * fps
 recording = False
-activity_count = 0
-yolo_count = 0
 ret, img = cap.read()
 if img.shape[1] / img.shape[0] > 1.55:
     res = (256, 144)
@@ -118,19 +93,16 @@ def receive_frames():
                     else:
                         time.sleep(5)
 
-
 # Functions for detecting key presses
 def press(key):
     global loop
     if key == 'q':
         loop = False
 
-
 def input_keyboard():
     listen_keyboard(
         on_press=press,
     )
-
 
 # Process YOLO object detection
 def process_yolo():
@@ -179,7 +151,6 @@ def process_yolo():
 
     return object_found, detected_object
 
-
 # Function to save images and save to database
 def save_image(image, detected_object):
     folder_name = datetime.now().strftime('%Y%m%d_%H%M%S')
@@ -217,7 +188,6 @@ def save_to_database(file_path, detected_object):
             cursor.close()
             connection.close()
 
-
 # Start the background threads
 receive_thread = threading.Thread(target=receive_frames)
 receive_thread.start()
@@ -225,7 +195,6 @@ keyboard_thread = threading.Thread(target=input_keyboard)
 keyboard_thread.start()
 
 # Main loop
-alpha = 0.5
 while loop:
     if not q.empty():
         img = q.get()
@@ -240,47 +209,26 @@ while loop:
             old_frame = np.float32(final_frame)
 
         # Use accumulateWeighted to reduce noise and get smoother frames
-        cv2.accumulateWeighted(final_frame, old_frame, alpha)
+        cv2.accumulateWeighted(final_frame, old_frame, 0.5)
         result_frame = cv2.convertScaleAbs(old_frame)
 
-        # Calculate difference between current and previous frame, then get ssim value
+        # Calculate difference between current and previous frame
         diff = cv2.absdiff(final_frame, result_frame)
         result = cv2.threshold(diff, 5, 255, cv2.THRESH_BINARY)[1]
-        ssim_val = int(ssim(result, blank))
 
-        # Print value for testing mode
-        if testing and ssim_val > thresh:
-            print("motion: " + str(ssim_val))
-
-        # Count the number of frames where the ssim value exceeds the threshold value.
-        # If the number of these frames exceeds start_frames value, run YOLO detection.
-        if ssim_val > thresh:
-            activity_count += 1
-            if activity_count >= start_frames:
-                if yolo_on:
-                    object_found, detected_object = process_yolo()
-                    if object_found:
-                        yolo_count += 1
-                        # Save image in a new thread
-                        threading.Thread(target=save_image, args=(img, detected_object)).start()
-                    else:
-                        yolo_count = 0
-        else:
-            activity_count = 0
-            yolo_count = 0
+        # Count the number of frames where the motion value exceeds the threshold value.
+        if np.sum(result) > 350:
+            if yolo_on:
+                object_found, detected_object = process_yolo()
+                if object_found:
+                    # Save image in a new thread
+                    threading.Thread(target=save_image, args=(img, detected_object)).start()
 
         # Monitor the stream
         if monitor:
             cv2.imshow(rtsp_stream, img)
-            if frame_click:
-                cv_key = cv2.waitKey(0) & 0xFF
-                if cv_key == ord("q"):
-                    loop = False
-                if cv_key == ord("n"):
-                    continue
-            else:
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    loop = False
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                loop = False
     else:
         time.sleep(period / 2)
 
